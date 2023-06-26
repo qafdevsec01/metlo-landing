02aebc94e6e71611a26d060d3abb5167b2fd62e7
Nikhil Shahi <nshahi1998@gmail.com>
2023-06-26 17:14:15 -0500

    remove outer div

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -162,32 +162,27 @@ const NavBar = () => {
           </div>
           <div className="flex items-center gap-6">
             {starCount > 0 ? (
-              <div
-                className="px-4 py-2 border border-slate-700 hidden lg:inline-flex rounded-md text-white gap-6 items-center"
-                style={{ backgroundColor: "rgb(0, 0, 0, 0.5)" }}
-              >
-                <p>Star us on GitHub</p>
-                <div className="grid grid-cols-2 text-sm">
-                  <a
-                    className="p-2 bg-[#1a1e23] hover:bg-gray-800 rounded-l-md inline-flex gap-2 items-center border border-slate-700 hover:border-slate-300"
-                    target="_blank"
-                    href="https://github.com/metlo-labs/metlo"
-                  >
-                    <div className="h-[16px] w-[16px] text-white">{star}</div>
-                    <p className="text-white">Stars</p>
-                  </a>
-                  <a
-                    className="p-2 rounded-r-md text-center text-white focus-within:text-metloblue hover:text-metloblue border-slate-800"
-                    style={{
-                      borderWidth: "1px",
-                      borderLeftWidth: "0",
-                    }}
-                    target="_blank"
-                    href="https://github.com/metlo-labs/metlo/stargazers"
-                  >
-                    <p>{starCount.toLocaleString()}</p>
-                  </a>
-                </div>
+              <div className="grid grid-cols-2 text-sm">
+                <a
+                  className="p-2 bg-[#1a1e23] hover:bg-gray-800 rounded-l-md inline-flex gap-2 items-center border border-slate-700 hover:border-slate-300"
+                  target="_blank"
+                  href="https://github.com/metlo-labs/metlo"
+                >
+                  <div className="h-[16px] w-[16px] text-white">{star}</div>
+                  <p className="text-white">Stars</p>
+                </a>
+                <a
+                  className="p-2 rounded-r-md text-center text-white focus-within:text-metloblue hover:text-metloblue border-slate-800"
+                  style={{
+                    borderWidth: "1px",
+                    borderLeftWidth: "0",
+                    backgroundColor: "rgb(0, 0, 0, 0.5)",
+                  }}
+                  target="_blank"
+                  href="https://github.com/metlo-labs/metlo/stargazers"
+                >
+                  <p>{starCount.toLocaleString()}</p>
+                </a>
               </div>
             ) : null}
             <Button href="https://app.metlo.com/login" variant="link">
