a731ac4bfaeab7af5a7f692dcda1449c7dc7bfa8
Nikhil Shahi <nshahi1998@gmail.com>
2023-06-26 17:11:16 -0500

    fix hover bg

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -169,7 +169,7 @@ const NavBar = () => {
                 <p>Star us on GitHub</p>
                 <div className="grid grid-cols-2 text-sm">
                   <a
-                    className="p-2 bg-[#1a1e23] hover:bg-gray-600 rounded-l-md inline-flex gap-2 items-center border border-slate-700 hover:border-slate-300"
+                    className="p-2 bg-[#1a1e23] hover:bg-gray-800 rounded-l-md inline-flex gap-2 items-center border border-slate-700 hover:border-slate-300"
                     target="_blank"
                     href="https://github.com/metlo-labs/metlo"
                   >
