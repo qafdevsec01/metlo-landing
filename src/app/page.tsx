6eff816b7388f882f13e74314497b9b5b7c33047
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-07-03 12:55:06 -0700

    add feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import Discovery from "@/components/Discovery";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
@@ -12,6 +13,9 @@ import BottomCTA from "@/components/BottomCTA";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
+      <Script>
+        {`(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('p1e024BBWMK4GB6d');`}
+      </Script>
       <NavBar />
       <VStack className="w-full" space={40}>
         <Hero />
